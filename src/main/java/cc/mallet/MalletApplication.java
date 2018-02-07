42a40fd44e7d35b41c4f5e240ebca681eb21619f
sh.baek <baek.sangha@navercorp.com>
2018-02-07 19:36:31 +0900

    mallet algorithm

---
+++ src/main/java/cc/mallet/MalletApplication.java
@@ -0,0 +1,12 @@
+package cc.mallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalletApplication.class, args);
+	}
+}
